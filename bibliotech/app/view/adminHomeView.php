167dc7fe02162d20880f2bca84e5411b9a15b79d
JosafatTHdz <20230005@uthh.edu.mx>
2024-07-22 11:48:52 -0600

    Delete bibliotech/app directory

--- bibliotech/app/view/adminHomeView.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h1>Bienvenido a nuestra pagina</h1>
-    <a href="#">Iniciar sesion</a>
-    <a href="http://localhost/bibliotech?C=registerController.php&M=index">Registrarte</a>
-</div>
